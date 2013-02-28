74bfd6257ed3281681a7a97ee6718a28bdf44a38
Ranjib Dey <ranjib@google.com>
2013-02-27 23:45:31 -0800

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "dey.ranjib@gmail.com"
 license           "Apache 2.0"
 description       "Multi stage chef runs. Run sub chef runs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.2"
+version           "0.0.3"
 
 %w{ debian ubuntu redhat centos fedora scientific amazon oracle}.each do |os|
   supports os
