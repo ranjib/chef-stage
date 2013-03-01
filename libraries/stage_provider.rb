66202d2c36ace313b4768f350738afbf0645c274
Ranjib Dey <ranjib@google.com>
2013-03-01 00:35:22 -0800

    moving to hwrp

---
+++ libraries/stage_provider.rb
@@ -0,0 +1,28 @@
+require 'chef/provider'
+
+class Chef
+  class Provider
+    class Stage < Chef::Provider
+
+      def whyrun_supported?
+        true
+      end
+
+      def action_run 
+        stage = @new_resource.name
+        Chef::Log.info("Executing stage: #{@new_resource.name} with runlist: '#{@new_resource.run_list}'")
+        staged_chef_client = Chef::StagedChefClient.new(stage, @new_resource.json_attributes, @new_resource.arguments)
+        staged_chef_client.run_chef_stage(@new_resource.run_list)
+        unless staged_chef_client.run_status.updated_resources.empty?
+          @new_resource.updated_by_last_action(true)
+        end
+
+        if @new_resource.save
+          Chef::Log.info("Executing stage: #{@new_resource.name} saving node")
+          node.consume_attributes(staged_chef_client.cleanse_attrs)
+        end
+      end
+    end
+  end
+end
+
---
+++ libraries/stage_resource.rb
@@ -0,0 +1,48 @@
+require 'chef/resource'
+
+class Chef
+  class Resource
+    class Stage < Chef::Resource
+
+      def initialize(name, collection=nil, node=nil)
+        super(name, collection, node)
+        @resource_name = :stage
+        @name = name
+        @save = true
+        @json_attributes = {}
+        @arguments = {}
+        @action = "run"
+        @allowed_actions.push(:run)
+      end
+
+      def name(arg=nil)
+        set_or_return(:name, arg, :kind_of => String)
+      end
+
+      def save(arg=nil)
+        set_or_return(:save, arg, :kind_of => [TrueClass, FalseClass])
+      end
+
+      def run_list(arg=nil)
+        set_or_return(:run_list, arg, :kind_of => String)
+      end
+
+      def json_attributes(arg=nil)
+        set_or_return(:json_attributes, arg, :kind_of => Hash)
+      end
+
+      def arguments(arg=nil)
+        set_or_return(:arguments, arg, :kind_of => Hash)
+      end
+      
+      def resources(&block)
+        if block_given?
+          @block = block
+        else
+          @block = nil
+        end
+      end
+      
+    end
+  end
+end
